a864c5212e527184810227f820813c021b2bb4a6
Max Metral <max@pyralis.com>
2017-02-01 10:01:49 -0500

    Add a psql-like bin script to remove our need for psql in most cases

---
+++ src/run-pg-sql.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+/* eslint-disable no-console */
+import fs from 'fs';
+import assert from 'assert';
+import pgp from 'pg-promise';
+
+const { PGHOST, PGUSER, PGPASSWORD } = process.env;
+
+assert(PGHOST, 'Must have PGHOST environment variable');
+assert(PGUSER, 'Must have PGUSER environment variable');
+assert(PGPASSWORD, 'Must have PGPASSWORD environment variable');
+
+const database = process.argv[2];
+const sql = fs.readFileSync(process.argv[3], 'utf8');
+
+const client = pgp();
+const connection = client({
+  host: PGHOST,
+  database,
+  user: PGUSER,
+  password: PGPASSWORD,
+});
+
+console.log(sql);
+connection.none(sql)
+  .then(() => {
+    console.log('Query completed.');
+    client.end();
+    process.exitCode = 0;
+  })
+  .catch((e) => {
+    console.error(e);
+    client.end();
+    process.exitCode = -1;
+  });
